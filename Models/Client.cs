4edaa1e393f535ea86b623933189e5fdb034e6b3
Richelle <richelle.ak@gmail.com>
2023-03-09 10:33:45 -0800

    add models for client, stylist, and context

---
+++ Models/Client.cs
@@ -0,0 +1,12 @@
+namespace HairSalon.Models
+{
+  public class Client
+  {
+    public int StylistId {get; set;}
+    public int ClientId {get; set;}
+    public string Name {get; set;}
+
+    public Stylist stylist {get; set;}
+
+  }
+}
