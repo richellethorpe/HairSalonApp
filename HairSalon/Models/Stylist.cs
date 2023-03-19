76464c73e4074627a03c8cfa92b657f0c8d69346
Richelle <richelle.ak@gmail.com>
2023-03-18 17:49:01 -0700

    add gitignore

---
+++ HairSalon/Models/Stylist.cs
@@ -0,0 +1,13 @@
+using System.Collections.Generic;
+
+namespace HairSalon.Models
+{
+  public class Stylist
+  {
+    public int StylistId { get; set; }
+
+    public string Name { get; set; }
+
+    public List<Client> Clients { get; set; }
+  }
+}
